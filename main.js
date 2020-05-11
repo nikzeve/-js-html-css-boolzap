8fb5dfb09fd0c7946f5772fcfbd8d2086fe78752
Nicola Maria Zeverino <nikzeve@gmail.com>
2020-05-11 20:09:38 +0200

    push intermedia

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 
-// $('#invia').click(function() {
-//     var nuovo_messaggio = $('#new_message').val();
-//     console.log(nuovo_messaggio);
-// })
+$('#invia').click(function() {
+    var nuovo_messaggio = $('#new_message').val();
+    console.log(nuovo_messaggio);
+
+    var new_text = $('.template .message, .template .sent').clone();
+
+    new_text.text(nuovo_messaggio);
+    $('#messagges-container').append(new_text);
+});
